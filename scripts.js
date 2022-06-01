7c01d633973d53ba8c5078243e6434fde315e831
Carlos Mayer <carlitos5525.cap@gmail.com>
2022-06-01 11:45:17 -0300

    exercicio 2

--- scripts.js
+++ scripts.js
@@ -11,3 +11,23 @@ function exerc_1(){
 
     document.getElementById("resultado").innerHTML = texto;
 }
+
+function exerc_2(){
+    genero_escolhido = document.getElementById('genero');
+    var genero = genero_escolhido.options[genero_escolhido.selectedIndex].value;
+    
+    altura_cm = document.getElementById('altura').value;
+    altura = altura_cm / 100;
+
+    var peso_ideal = 0;
+
+    if(genero == "masc"){
+        peso_ideal = (72.7 * altura) - 58;
+    }
+    else{
+        peso_ideal = (62.1 * altura) - 44.7;
+    }
+
+    var texto = "O peso ideal é " + peso_ideal + "kg.";
+    document.getElementById("resultado").innerHTML = texto;
+}
